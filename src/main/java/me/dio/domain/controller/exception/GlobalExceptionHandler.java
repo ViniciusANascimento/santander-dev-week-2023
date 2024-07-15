c48ce1ef75c546b74a8a67957cc93bee820cc958
Vinicius Nascimento <vini.a.nasci@gmail.com>
2024-07-15 11:27:14 -0300

    feat: exceptions and services.

---
+++ src/main/java/me/dio/domain/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package me.dio.domain.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBussinessException(IllegalArgumentException bussinessException){
+
+        return new ResponseEntity<>(bussinessException.getLocalizedMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException){
+
+        return new ResponseEntity<>("Resource ID not Found", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException){
+        var message = "Unexpected serve error, see the logs";
+        logger.error("",unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
